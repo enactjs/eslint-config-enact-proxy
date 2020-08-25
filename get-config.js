be801d5b8a49c05a50be8391b4bed9e60ec3d679
Jason Robitaille <robitaille88@gmail.com>
2020-08-25 08:08:49 -0700

    ENYO-6491: CLI-based ESLint plugins/parser not always resolved correctly

--- get-config.js
+++ get-config.js
@@ -2,35 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const {npmGlobalModules, yarnGlobalModules, supportGlobalResolving} = require('./global-resolver');
 
-const globalModulesPath = npmGlobalModules() || yarnGlobalModules() || '';
+const globalPaths = [npmGlobalModules(), yarnGlobalModules()].filter(Boolean);
+const defaultSearch = globalPaths
+	.reduce((acc, dir) => (acc.concat(dir, path.join(dir, '@enact', 'cli', 'node_modules'))), [])
+	.filter(dir => fs.existsSync(dir));
 
 function getGlobalConfig({
-		search = [
-			globalModulesPath,
-			path.join(globalModulesPath, '@enact', 'cli', 'node_modules')
-		],
+		search = defaultSearch,
 		ruleset = 'enact'
 	} = {}
 ) {
-	if (globalModulesPath) {
-		// Locate ESLint root package level from active main module
-		const eslintPath = path.join(globalModulesPath, 'eslint')
-
-
-		// ESLint's module resolver file
-		const resolverFile = path.join(eslintPath, 'lib', 'shared', 'relative-module-resolver.js');
-
-		if (fs.existsSync(resolverFile)) {
-			// Find first global search path containing eslint-config-enact
-			const dir = search.find(d => fs.existsSync(path.join(d, 'eslint-config-enact')));
-			if (dir) supportGlobalResolving(resolverFile, dir);
-		}
-		// If eslint/lib/shared/relative-module-resolver.js does not exist, then
-		// our global resolving support cannot be patched in.
-		// As a meaningful fallback, and to backward-support ESLint <=5.x, still
-		// should extend the desired ruleset.
-		return {extends: ruleset};
+	// Locate ESLint module resolver file
+	const eslintResolverPath = globalPaths
+		.map(dir => path.join(dir, 'eslint', 'lib', 'shared', 'relative-module-resolver.js'))
+		.find(dir => fs.existsSync(dir));
+	if (eslintResolverPath) {
+		supportGlobalResolving(eslintResolverPath, search);
 	}
+	// If eslint/lib/shared/relative-module-resolver.js does not exist, then
+	// our global resolving support cannot be patched in.
+	// As a meaningful fallback, and to backward-support ESLint <=5.x, still
+	// should extend the desired ruleset.
+	return {extends: ruleset};
 }
 
 module.exports = {
