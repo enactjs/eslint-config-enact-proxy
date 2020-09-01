0c877eb46736db69ec8cd5086dc17b531a9aa7a6
Jason Robitaille <robitaille88@gmail.com>
2020-09-01 07:10:30 -0700

    Improved dynamic detection of NPM or Yarn global modules depending on whether the invoking ESLint was NPM or Yarn based.

--- get-config.js
+++ get-config.js
@@ -2,8 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const {npmGlobalModules, yarnGlobalModules, supportGlobalResolving} = require('./global-resolver');
 
+// Find first parent module from the accessing ESLint.
+const parentOf = m => Object.values(require.cache).filter(c => c.children.includes(m))[0];
+const eslintParent = parentOf(parentOf(module));
+
+// Detect NPM and Yarn global node_modules locations and attempt to find an active entry.
 const globalPaths = [npmGlobalModules(), yarnGlobalModules()].filter(Boolean);
-const defaultSearch = globalPaths
+const activeGlobalPath = globalPaths.find(d =>
+	eslintParent.path.startsWith(fs.realpathSync(path.dirname(d)))
+);
+
+// Use base global node_modules location and <global>/@enact/cli/node_modules as the default
+// search locations.
+const defaultSearch = (activeGlobalPath ? [activeGlobalPath] : globalPaths)
 	.reduce((acc, dir) => (acc.concat(dir, path.join(dir, '@enact', 'cli', 'node_modules'))), [])
 	.filter(dir => fs.existsSync(dir));
 
@@ -12,8 +23,8 @@ function getGlobalConfig({
 		ruleset = 'enact'
 	} = {}
 ) {
-	// Locate ESLint module resolver file
-	const eslintResolverPath = globalPaths
+	// Locate ESLint module resolver file.
+	const eslintResolverPath = search
 		.map(dir => path.join(dir, 'eslint', 'lib', 'shared', 'relative-module-resolver.js'))
 		.find(dir => fs.existsSync(dir));
 	if (eslintResolverPath) {
